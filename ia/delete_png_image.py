d0be72db458a3b3b094847291aaca8e7fa0a37bb
Vinicius Giovanini <vgiovanini@sga.pucminas.br>
2023-10-12 17:36:39 -0300

    add image processing

---
+++ ia/delete_png_image.py
@@ -0,0 +1,29 @@
+import os
+from PIL import Image
+
+folder_path = './data/archiveshort/data/'
+
+
+image_paths = []
+large_image_path = []
+
+for root, dirs, files in os.walk(folder_path):
+    for file in files:
+
+        image_path = os.path.join(root, file)
+
+        try:
+            img = Image.open(image_path)
+        except:
+            large_image_path.append(image_path)
+
+        if file.lower().endswith('.png'):
+            image_paths.append(image_path)
+
+# print(image_paths)
+# print(large_image_path)
+
+for each in image_paths:
+    os.remove(each)
+for each in large_image_path:
+    os.remove(each)
